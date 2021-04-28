f947b38269799fe5f1fe19b7e87c7890273b3647
GrassTidesTest <miroslav.hinner@devpack.cz>
2021-04-28 09:14:44 +0200

    using firefox instead of chrome

--- src/test/java/base/WebDriverSingleton.java
+++ src/test/java/base/WebDriverSingleton.java
@@ -2,18 +2,18 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverSingleton {
     private WebDriver driver;
     private static WebDriverSingleton instance;
 
     public void initialize() {
-        initializeGoogleChrome();
+        initializeMozillaFirefox();
     }
 
-    public static WebDriverSingleton getInstance(){
-        if (instance == null){
+    public static WebDriverSingleton getInstance() {
+        if (instance == null) {
             instance = new WebDriverSingleton();
         }
         return instance;
@@ -22,6 +22,11 @@ public class WebDriverSingleton {
     private void initializeGoogleChrome() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
         driver = new ChromeDriver();
+    }
+
+    private void initializeMozillaFirefox() {
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
     }
 
     public WebDriver getDriver() {
