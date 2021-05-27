9e5886aaca350033fce293dd7280932a530628d2
GrassTidesTest <miroslav.hinner@devpack.cz>
2021-05-27 13:24:28 +0200

    printing timestamp/link finished

--- src/test/java/pages/ProfesiaCzPage.java
+++ src/test/java/pages/ProfesiaCzPage.java
@@ -1,16 +1,15 @@
 package pages;
 
 import base.WebDriverSingleton;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class ProfesiaCzPage {
     private WebDriver driver;
@@ -27,10 +26,16 @@ public class ProfesiaCzPage {
     // Filter positions page
     private static final String IT_PROFESSION = "IT";
     private static final String FULLTIME_CHECKBOX_XP = "//label[text()[contains(.,'plný úvazek')]]";
+    private static final String COOKIES_BTN_CSS = "button[aria-label='NESOUHLASÍM']";
 
     // Page with positions constants
     private static final String POSITIONS_XPATH = "//li[@class='list-row']";
     private static final String NEXT_PAGE_BUTTON_CSS = "ul.pagination a.next";
+    private static final String TIMESTAMP_PATTERN = "dd.MM.yyyy HH:mm";
+    //    private static final String POSITION_NAME_XPATH = "//h3[contains(@class,'title')]";
+//    private static final String POSITION_COMPANY_XPATH = "//div[contains(@class,'company')]";
+//    private static final String POSITION_LINK_XPATH = "//h3/a";
+    private static final String POSITION_LINK_CSS = "h2 > a[id]";
 
     @FindBy(id = "offerCriteriaSuggesterInputId")
     private WebElement professionFieldTextbox;
@@ -62,18 +67,20 @@ public class ProfesiaCzPage {
     }
 
     public void closeCookiesMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_IN_SECS);
+
         // wait for the page to load
-        new WebDriverWait(driver,5);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(COOKIES_BTN_CSS)));
 
         // if the cookies message appears disable it
-        if (driver.findElements(By.cssSelector("button[aria-label='NESOUHLASÍM']")).size() > 0) {
-            driver.findElement(By.cssSelector("button[aria-label='NESOUHLASÍM']")).click();
+        if (driver.findElements(By.cssSelector(COOKIES_BTN_CSS)).size() > 0) {
+            driver.findElement(By.cssSelector(COOKIES_BTN_CSS)).click();
         }
     }
 
     public void enterProfessionField() throws InterruptedException {
         // wait for the page to load
-        waitForVisibilityOfElement(driver, professionFieldTextbox);
+        waitUntilClickable(driver, professionFieldTextbox);
 
         // type 'IT' into the textbox and hit enter to select the IT from the suggested options
         professionFieldTextbox.sendKeys(IT_PROFESSION);
@@ -82,9 +89,10 @@ public class ProfesiaCzPage {
     }
 
     public void selectFullTimeJob() {
-        // find
+        // find the checkbox
         WebElement fullTimeCheckbox = jobTypeElement.findElement(By.xpath(FULLTIME_CHECKBOX_XP));
 
+        // if not checked, check it
         if (!fullTimeCheckbox.isSelected()) {
             fullTimeCheckbox.click();
         }
@@ -139,13 +147,73 @@ public class ProfesiaCzPage {
 
             // send positions size to the function
             // go through the list of positions and save them to excel
-//            getPositionsAndSaveThemToExcel(positions_size);
-            System.out.println(positions_size);
+            getPositionsAndSaveThemToExcel(positions_size);
+//            System.out.println(positions_size);
 
             // if the code reaches the last page or the set limit, break the cycle
             if (getBreakCondition(i)) break;
         }
     }
+
+    private void getPositionsAndSaveThemToExcel(int positions_size) throws IOException {
+
+        // go through the list of positions and for each position determine
+        // if the basic info or detail info will be saved to the excel
+        for (int i = 1; i < positions_size + 1; i++) {
+
+            // wait for the page to load
+            waitUntilClickable(driver, listElement);
+
+            // get webElement of the current position
+            // use i in xpath to determine which position from the list to work with
+            // i = 1, means it's the first position from the list etc.
+            WebElement position = driver.findElement(By.xpath(POSITIONS_XPATH + "[" + i + "]"));
+
+            // get basic information for the position and save it to corresponding variables
+            String timestamp = getTimeStamp(TIMESTAMP_PATTERN);
+//            String positionName = getElementText(position, POSITION_NAME_XPATH);
+//            String company = getElementText(position, POSITION_COMPANY_XPATH);
+//            String linkAddress = cropPositionLink(position, POSITION_LINK_XPATH);
+            String linkAddress = getPositionLinkCss(position, POSITION_LINK_CSS);
+//            String salaryValue = getLabelValue(position, SALARY_LABEL_XPATH);
+//            String homeOfficeValue = getLabelValue(position, HOMEOFFICE_LABEL_XPATH);
+
+            System.out.println(timestamp);
+            System.out.println(linkAddress);
+
+            // open new tab and create ArrayList with windowHandles
+//            ((JavascriptExecutor) driver).executeScript("window.open()");
+//            ArrayList<String> currentTabs = new ArrayList<String>(driver.getWindowHandles());
+//
+//            // open the current position in new tab
+//            // by doing this we avoid getting stuck by some aggressive popups when closing the position page
+//            openLinkInTab(linkAddress, currentTabs);
+//
+//            // get position level and decide what to do with the position
+//            switch (getPositionLevel(linkAddress)) {
+//                case BASIC:
+//                case MEDIUM:
+//                    // get the detailed information from the page for BASIC and MEDIUM
+//                    // print what info from which position you save
+//                    System.out.println("Detail info: " + positionName);
+//                    getDetailedInformation(timestamp, positionName, company, linkAddress, homeOfficeValue);
+//                    break;
+//                case ADVANCED:
+//                    // otherwise save basic info to the excel, reading it from different layouts is not an option here
+//                    // print what info from which position you save
+//                    System.out.println("Basic info: " + positionName);
+//                    ExcelWriter(timestamp, positionName, company, linkAddress, salaryValue, homeOfficeValue);
+//                    break;
+//                case CLOSED:
+//                    // position is closed and there is nothing we can do
+//                    System.out.println("Closed: " + positionName);
+//            }
+//
+            // close the current tab with the position and focus back on the position list page
+//            closeTabWithPosition(currentTabs);
+        }
+    }
+
 
     private boolean getBreakCondition(int i) {
         boolean isForwardButtonPresent;
@@ -176,5 +244,69 @@ public class ProfesiaCzPage {
     private void waitForVisibilityOfElement(WebDriver driver, WebElement webElement) {
         new WebDriverWait(driver, TIMEOUT_IN_SECS)
                 .until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+    private void waitUntilClickable(WebDriver driver, WebElement webElement) {
+        new WebDriverWait(driver, TIMEOUT_IN_SECS)
+                .until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    private String getTimeStamp(String pattern) {
+        // return time stamp based on a pattern
+        return new SimpleDateFormat(pattern).format(new java.util.Date());
+    }
+
+    private String getElementText(WebElement parentElement, String xpath) {
+        // return text of the element based on the parentElement and xpath
+        return parentElement.findElement(By.xpath("." + xpath)).getText();
+    }
+
+    private String getCssElementText(WebElement parentElement, String cssSel) {
+        // return text of the element based on the parentElement and css selector
+        return parentElement.findElement(By.cssSelector(cssSel)).getText();
+    }
+
+    private String cropPositionLink(WebElement parentElement, String xpath) {
+        // get the href value from the element
+        String originalString = parentElement.findElement(By.xpath("." + xpath)).getAttribute("href");
+
+        // crop the link so that the part after ? including is not present
+        // link must contain ? otherwise it fails
+        return originalString.substring(0, originalString.indexOf("?"));
+    }
+
+    private String getPositionLinkCss(WebElement parentElement, String cssSel) {
+        // get the href value from the element
+        return parentElement.findElement(By.cssSelector(cssSel)).getAttribute("href");
+    }
+
+    private String getLabelValue(WebElement parentElement, String xpath) {
+        // define empty string
+        String value = "";
+
+        // if the element exists
+        if (parentElement.findElements(By.xpath("." + xpath)).size() > 0) {
+
+            // save it's value to the string
+            value = parentElement.findElement(By.xpath("." + xpath)).getText().trim();
+        }
+
+        return value;
+    }
+
+    private void openLinkInTab(String positionLink, ArrayList<String> tabs) {
+        // change focus to new window
+        driver.switchTo().window(tabs.get(1));
+
+        // open new link
+        driver.get(positionLink);
+    }
+
+    private void closeTabWithPosition(ArrayList<String> tabs) {
+        // close the tab
+        driver.close();
+
+        // change back to main windows
+        driver.switchTo().window(tabs.get(0));
     }
 }
