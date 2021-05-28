ded711d9409104e9d2b5412ee496efe771c23bb3
GrassTidesTest <miroslav.hinner@devpack.cz>
2021-05-28 14:26:05 +0200

    semi-detailed info printout finished

--- src/test/java/pages/ProfesiaCzPage.java
+++ src/test/java/pages/ProfesiaCzPage.java
@@ -22,7 +22,7 @@ public class ProfesiaCzPage {
     private static final String FILE_NAME = "data.xlsx";
 
     private static final int NUMBER_OF_PAGES_TO_CHECK = 20;
-    private static final int TIMEOUT_IN_SECS = 5;
+    private static final int TIMEOUT_IN_SECS = 10;
 
     // Filter positions page
     private static final String IT_PROFESSION = "IT";
@@ -36,6 +36,10 @@ public class ProfesiaCzPage {
     private static final String POSITION_LINK_CSS = "h2 > a[id]";
     private static final String POSITION_COMPANY_CSS = ".employer";
     private static final String SALARY_LABEL_CSS = "a[data-dimension7='Salary label']";
+
+    // Page of the position
+    private static final String POSITION_CONTACT_ELEMENT_XP = "//*[contains(text(),'Kontaktní osoba:')]";
+    private static final String EMPLOYMENT_TYPE_CSS = "[itemprop='employmentType']";
 
     @FindBy(id = "offerCriteriaSuggesterInputId")
     private WebElement professionFieldTextbox;
@@ -54,6 +58,12 @@ public class ProfesiaCzPage {
 
     @FindBy(css = NEXT_PAGE_BUTTON_CSS)
     private WebElement nextPageButton;
+
+    @FindBy(xpath = POSITION_CONTACT_ELEMENT_XP)
+    private WebElement contactElement;
+
+    @FindBy(id = "detail")
+    private WebElement positionMainElement;
 
     public ProfesiaCzPage() {
         driver = WebDriverSingleton.getInstance().getDriver();
@@ -175,23 +185,22 @@ public class ProfesiaCzPage {
             String company = getElementTextCss(position, POSITION_COMPANY_CSS);
             String linkAddress = getPositionLinkCss(position, POSITION_LINK_CSS);
             String salaryValue = getLabelValueCss(position, SALARY_LABEL_CSS);
-//            String homeOfficeValue = getLabelValue(position, HOMEOFFICE_LABEL_XPATH);
 
             System.out.println(timestamp);
             System.out.println(positionName);
             System.out.println(linkAddress);
             System.out.println(company);
             System.out.println(salaryValue);
-            System.out.println("-----------------------------------");
+            System.out.println("----------------------------");
 
 
             // open new tab and create ArrayList with windowHandles
-//            ((JavascriptExecutor) driver).executeScript("window.open()");
-//            ArrayList<String> currentTabs = new ArrayList<String>(driver.getWindowHandles());
-//
-//            // open the current position in new tab
-//            // by doing this we avoid getting stuck by some aggressive popups when closing the position page
-//            openLinkInTab(linkAddress, currentTabs);
+            ((JavascriptExecutor) driver).executeScript("window.open()");
+            ArrayList<String> currentTabs = new ArrayList<String>(driver.getWindowHandles());
+
+            // open the current position in new tab
+            // by doing this we avoid getting stuck by some aggressive popups when closing the position page
+            openLinkInTab(linkAddress, currentTabs);
 
 //            // get position level and decide what to do with the position
 //            switch (getPositionLevel(linkAddress)) {
@@ -200,13 +209,13 @@ public class ProfesiaCzPage {
 //                    // get the detailed information from the page for BASIC and MEDIUM
 //                    // print what info from which position you save
 //                    System.out.println("Detail info: " + positionName);
-//                    getDetailedInformation(timestamp, positionName, company, linkAddress, homeOfficeValue);
+            getDetailedInformation(timestamp, positionName, company, linkAddress);
 //                    break;
 //                case ADVANCED:
 //                    // otherwise save basic info to the excel, reading it from different layouts is not an option here
 //                    // print what info from which position you save
 //                    System.out.println("Basic info: " + positionName);
-                    ExcelWriter(timestamp, positionName, company, linkAddress, salaryValue);
+//                    ExcelWriter(timestamp, positionName, company, linkAddress, salaryValue);
 //                    break;
 //                case CLOSED:
 //                    // position is closed and there is nothing we can do
@@ -214,7 +223,7 @@ public class ProfesiaCzPage {
 //            }
 //
             // close the current tab with the position and focus back on the position list page
-//            closeTabWithPosition(currentTabs);
+            closeTabWithPosition(currentTabs);
         }
     }
 
@@ -239,6 +248,119 @@ public class ProfesiaCzPage {
         // return false so that the cycle can repeat
         return false;
     }
+
+    private void getDetailedInformation(String timestamp, String positionName, String companyValue, String link)
+            throws IOException {
+
+        String[] contactInfo;
+        String typeOfEmployment = "";
+
+        // wait for the page to load
+        waitForVisibilityOfElement(driver, positionMainElement);
+
+        if (driver.findElements(By.cssSelector(EMPLOYMENT_TYPE_CSS)).size() > 0) {
+            typeOfEmployment = driver.findElement(By.cssSelector(EMPLOYMENT_TYPE_CSS)).getText();
+        }
+
+        System.out.println("Employment type: " + typeOfEmployment);
+
+
+        if (driver.findElements(By.xpath(POSITION_CONTACT_ELEMENT_XP)).size() > 0) {
+            contactInfo = getContactInformation(contactElement.getText());
+
+            System.out.println("Contact name: " + contactInfo[0]);
+            System.out.println("Contact phone: " + contactInfo[1]);
+            System.out.println("Contact mail: " + contactInfo[2]);
+        } else {
+            System.out.println("NO CONTACT INFO");
+        }
+        System.out.println("------------------------------------------------------------------------------------");
+
+
+        // make sure the element exists before taking the info out of it
+//        if (doesElementExist(DETAIL_INFO_XPATH)) {
+
+        // get contact name and phone information
+//            contactName = getContactInfo(CONTACT_NAME_XPATH);
+//            contactPhone = getContactInfo(CONTACT_INFO_XPATH);
+
+        // determine which language is used and save the info to variables
+//            switch (determineLanguage(DETAIL_INFO_XPATH)) {
+
+        // if the language is CZECH, use CZ text
+//                case "CZECH":
+//                    education = getInformationText(EDUCATION_TEXT_CZ);
+//                    languages = getInformationText(LANGUAGES_TEXT_CZ);
+//                    salary = getInformationText(SALARY_TEXT_CZ);
+//                    benefits = getInformationText(BENEFITS_TEXT_CZ);
+//                    typeOfEmployment = getInformationText(EMPLOYMENT_FORM_TEXT_CZ);
+//                    typeOfContract = getInformationText(TYPE_OF_CONTRACT_TEXT_CZ);
+//                    authority = getInformationText(EMPLOYER_TEXT_CZ);
+//                    break;
+
+        // if the language is ENGLISH, use EN text
+//                case "ENGLISH":
+//                    education = getInformationText(EDUCATION_TEXT_EN);
+//                    languages = getInformationText(LANGUAGES_TEXT_EN);
+//                    salary = getInformationText(SALARY_TEXT_EN);
+//                    benefits = getInformationText(BENEFITS_TEXT_EN);
+//                    typeOfEmployment = getInformationText(EMPLOYMENT_FORM_TEXT_EN);
+//                    typeOfContract = getInformationText(TYPE_OF_CONTRACT_TEXT_EN);
+//                    authority = getInformationText(EMPLOYER_TEXT_EN);
+//                    break;
+        // if the language is UNKNOWN, don't bother and save UNKNOWN only
+//                case "UNKNOWN":
+//                    education = UNKNOWN_LANGUAGE;
+//                    languages = UNKNOWN_LANGUAGE;
+//                    salary = UNKNOWN_LANGUAGE;
+//                    benefits = UNKNOWN_LANGUAGE;
+//                    typeOfEmployment = UNKNOWN_LANGUAGE;
+//                    typeOfContract = UNKNOWN_LANGUAGE;
+//                    authority = UNKNOWN_LANGUAGE;
+//                    break;
+//            }
+
+        // write values to Excel
+//            ExcelWriter(timestamp, positionName, companyValue, link, salary, homeOfficeValue, contactName, contactPhone,
+//                    education, languages, salary, benefits, typeOfEmployment, typeOfContract, authority);
+//        } else {
+//            System.out.println("this should not happen - it means that the position was determined as detailed " +
+//                    "but the detailed info is not on the page (possibly operator changed the code of the page)");
+//        }
+    }
+
+    private String[] getContactInformation(String originalString) {
+        // split original string to an array
+        String[] originalStrings = originalString.split("\n");
+
+        // declare new string array with three elements
+        String[] strings = new String[]{"", "", ""};
+
+        // iterate through originalStrings
+        for (String string : originalStrings) {
+
+            // manipulate string and get only the information you desire
+            String substring = string.substring(string.indexOf(":") + 2);
+
+            // if string contains "Kontaktní osoba" it's contact name = strings[0]
+            if (string.contains("Kontaktní osoba")) {
+                strings[0] = substring;
+            }
+
+            // if string contains "Tel." it's contact phone = strings[1]
+            if (string.contains("Tel.")) {
+                strings[1] = substring;
+            }
+
+            // if string contains "Email" and not "životopis" it's contact email = strings[2]
+            if (string.contains("E-mail") && !string.contains("životopis")) {
+                strings[2] = substring;
+            }
+        }
+
+        return strings;
+    }
+
 
     public void recheckAllInputs() {
 
